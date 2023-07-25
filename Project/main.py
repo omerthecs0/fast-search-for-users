43ba661e9cbc038766f9a6298903fc6a09d9eaef
omerthecs0 <116056620+omerthecs0@users.noreply.github.com>
2023-07-26 01:00:33 +0300

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,35 +1,33 @@
-import requests
-from bs4 import BeautifulSoup
-import trendyol
-
-
 
 class Product:
-    def __init__(self, kategori="", marka="", ilanismi="", fiyat="", seller="", seller_point="", other_sellers="", yorum="", soru="", genel=""):
+    def __init__(self, kategori="", marka="", ilanismi="", fiyat="", seller="", other_sellers="", ratings="", reviews="", answers="", genel=""):
         self.kategori = kategori
         self.marka = marka
         self.ilanismi = ilanismi
         self.fiyat = fiyat
         self.seller = seller
-        self.seller_point = seller_point
         self.other_sellers = other_sellers
-        self.yorum = yorum
-        self.soru = soru
+        self.ratings = ratings
+        self.reviews = reviews
+        self.answers = answers
         self.genel = genel
 
+
+import trendyol
+import hepsiburada
+import teknosa
 
 
 def main():
     search = take_search()
     len_search = len(search.split(" "))
     trendyol.main(search, len_search)
+    hepsiburada.main(search)
+    teknosa.main(search)
 
 
 def take_search():
     return input("Enter your search: ")
-
-
-
 
 
 if __name__ == "__main__":
