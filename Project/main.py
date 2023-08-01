5305265e4bfca2a2c32f6de74048fa54f9594dcd
omerthecs0 <116056620+omerthecs0@users.noreply.github.com>
2023-08-01 15:47:45 +0300

    Update main.py

--- Project/main.py
+++ Project/main.py
@@ -1,35 +1,37 @@
-
+##Class for products.
 class Product:
-    def __init__(self, kategori="", marka="", ilanismi="", fiyat="", seller="", other_sellers="", ratings="", reviews="", answers="", genel=""):
+    def __init__(self, kategori="", marka="", ilanismi="", fiyat="", seller="", variants="", ratings="", reviews="", answers="", genel="", other_sellers=""):
         self.kategori = kategori
         self.marka = marka
         self.ilanismi = ilanismi
         self.fiyat = fiyat
         self.seller = seller
-        self.other_sellers = other_sellers
+        self.variants = variants
         self.ratings = ratings
         self.reviews = reviews
         self.answers = answers
         self.genel = genel
+        self.other_sellers = other_sellers
 
 
+##Import e-comemrce site product finder files.
 import trendyol
 import hepsiburada
 import teknosa
 
-
-
+##Main, initialization function.
 def main():
-    search = take_search()
-    len_search = len(search.split(" "))
-    trendyol.main(search, len_search)
+    search = take_search() ## Take search text from user.
+
+    ## Initialize the files to take products info
+    trendyol.main(search)
     hepsiburada.main(search)
     teknosa.main(search)
 
 
+## Take search from user.
 def take_search():
     return input("Enter your search: ")
-
 
 
 
